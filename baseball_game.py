560d27bc7b1b4e7dc798ac24b6ca5b4b4dc0681c
Maroo KH <hanmaroo.kim@gmail.com>
2021-01-19 15:48:00 +0900

    'update'

--- baseball_game.py
+++ baseball_game.py
@@ -84,9 +84,9 @@ def is_duplicated_number(three_digit):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-
-    result = three_digit.count(three_digit[0]) + \
-        three_digit.count(three_digit[1]) + three_digit.count(three_digit[2])
+    result = 0
+    for i in three_digit:
+        result += three_digit.count(i)
     # ==================================
     return result != 3
 
